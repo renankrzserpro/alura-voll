3d81163e56dd87fc699326fb2b4998ccc6964d2e
Renan Krzesinski <renan.krzesinski@serpro.gov.br>
2024-04-29 13:31:31 -0300

    use repository to create doctor

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -1,20 +1,26 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import med.voll.api.domain.Doctor;
 import med.voll.api.dto.DoctorDTO;
+import med.voll.api.repository.DoctorRepository;
 
 @RestController
 @RequestMapping("/doctors")
 public class DoctorController {
 
+    @Autowired
+    private DoctorRepository repository;
+
     @PostMapping
-    public String create(@RequestBody DoctorDTO d) {
-        return "Doctor: " + d;
+    public void create(@RequestBody DoctorDTO d) {
+        repository.save(new Doctor(d));
     }
 
     @GetMapping
