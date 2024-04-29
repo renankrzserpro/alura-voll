71e0c2c7102365f7019da9856e73639c4832099e
Renan Krzesinski <renan.krzesinski@serpro.gov.br>
2024-04-29 14:11:08 -0300

    add migration to create table patients

---
+++ src/main/resources/db/migration/V2__create-table-patients.sql
@@ -0,0 +1,9 @@
+CREATE TABLE patients (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    street_address VARCHAR(100) NOT NULL,
+    address_line_2 VARCHAR(100),
+    city VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY(ID)
+);
