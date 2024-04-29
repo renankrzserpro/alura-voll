6c7d35c4449a8e815a1bd0e1a1605ce0a0910651
Renan Krzesinski <renan.krzesinski@serpro.gov.br>
2024-04-29 14:09:29 -0300

    use repository to create patient

--- src/main/java/med/voll/api/controller/PatientController.java
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -1,20 +1,29 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
+import med.voll.api.domain.Patient;
 import med.voll.api.dto.PatientDTO;
+import med.voll.api.repository.PatientRepository;
 
 @RestController
 @RequestMapping("patients")
 public class PatientController {
 
+    @Autowired
+    private PatientRepository repository;
+
     @PostMapping
-    public String create(@RequestBody PatientDTO p) {
-        return "Patient: " + p;
+    @Transactional
+    public void create(@RequestBody @Valid PatientDTO p) {
+        repository.save(new Patient(p));
     }
 
     @GetMapping
